4c950f6e8bf6683bba6738e76186539ba41dcc5e
GustavoQueiros <gustavo.dev47@gmail.com>
2024-05-28 15:37:22 -0300

    Dockerfile deploy

---
+++ todolist/src/main/java/br/com/gustavoqueiros/todolist/user/IUserRepository.java
@@ -0,0 +1,11 @@
+package br.com.gustavoqueiros.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    UserModel findByUsername(String username);
+}
